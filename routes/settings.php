e9f69bb6e6152ac32cc60b17c7f02ed63bcfd313
joashneves <joashneves@gmail.com>
2025-04-18 19:28:25 -0300

    x

--- routes/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Settings\PasswordController;
-use App\Http\Controllers\Settings\ProfileController;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::resource('photos', PhotoController::class);
